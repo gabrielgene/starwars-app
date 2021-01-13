8d36b3c9f7eb61d66f3231a6be589c8533c86634
Gabriel Gene <gabrielgene97@gmail.com>
2021-01-13 08:51:10 -0300

    Add peopleList screen

--- src/screens/PeopleList/index.tsx
+++ src/screens/PeopleList/index.tsx
@@ -1,12 +1,54 @@
-import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {SafeAreaView, FlatList, StyleSheet} from 'react-native';
+import Loading from '@components/Loading';
+import PeopleItem from '@components/PeopleItem';
+import {useStoreApplicationContext} from '@context/storeApplication';
 
 export default function PeopleList() {
+  const {peopleList, loadPeople, loading} = useStoreApplicationContext();
+
+  useEffect(() => {
+    loadPeople();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <SafeAreaView>
-      <View style={{flex: 1}}>
-        <Text style={{color: '#000'}}>People List</Text>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        style={styles.flatList}
+        contentContainerStyle={styles.list}
+        data={peopleList}
+        renderItem={({item: {name, favorite}, index}) => (
+          <PeopleItem name={name} favorite={favorite} idx={index} />
+        )}
+        keyExtractor={(item) => item.name}
+        onEndReached={loadPeople}
+        onEndReachedThreshold={0.1}
+        ListFooterComponent={() => <Loading loading={loading} />}
+      />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#272b30',
+    flex: 1,
+  },
+  flatList: {
+    marginTop: 16,
+  },
+  list: {
+    paddingHorizontal: 20,
+  },
+
+  listItem: {
+    backgroundColor: '#333',
+    marginTop: 20,
+    padding: 30,
+  },
+  loading: {
+    alignSelf: 'center',
+    marginVertical: 20,
+  },
+});
